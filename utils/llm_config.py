e4b97e43da063ad0496c0e983ebaa21ad4f715a2
Breno Braido <81990426+BrenoSB98@users.noreply.github.com>
2025-04-18 17:15:26 -0300

    Commented lines

--- utils/llm_config.py
+++ utils/llm_config.py
@@ -1,44 +1,44 @@
-import streamlit as st
+# import streamlit as st
 
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains.retrieval import create_retrieval_chain
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
+# from langchain.chains.combine_documents import create_stuff_documents_chain
+# from langchain.chains.retrieval import create_retrieval_chain
+# from langchain_core.prompts import ChatPromptTemplate
+# from langchain_openai import ChatOpenAI
 
-def llm(key, model, query, vector_store):
-    llm_model = ChatOpenAI(
-        api_key=key,
-        model=model,
-        temperature= 0.5,
-        verbose=True,
-    )
-    retriever = vector_store.as_retriever()
+# def llm(key, model, query, vector_store):
+#     llm_model = ChatOpenAI(
+#         api_key=key,
+#         model=model,
+#         temperature= 0.5,
+#         verbose=True,
+#     )
+#     retriever = vector_store.as_retriever()
 
-    system_prompt = '''
-        Você é um assistente inteligente especializado na leitura e interpretação de arquivos PDF.  
-        Todas as suas respostas devem ser em Português Brasileiro e no formato Markdown para facilitar a leitura.
-        Responda às perguntas com base no contexto e no conteúdo dos arquivos PDF fornecidos.  
-        Se a informação estiver disponível, informe também o número da página onde ela pode ser encontrada (ex: Pag. 05).  
-        Caso a resposta não esteja presente no documento, diga claramente que a informação não foi localizada.
-        Priorize respostas claras, diretas e bem estruturadas. Utilize listas, tabelas, negrito, itálico e cabeçalhos Markdown sempre que apropriado.  
-        Evite suposições ou interpretações além do que está explícito no documento.
-        Contexto: {context}
-    '''
+#     system_prompt = '''
+#         Você é um assistente inteligente especializado na leitura e interpretação de arquivos PDF.  
+#         Todas as suas respostas devem ser em Português Brasileiro e no formato Markdown para facilitar a leitura.
+#         Responda às perguntas com base no contexto e no conteúdo dos arquivos PDF fornecidos.  
+#         Se a informação estiver disponível, informe também o número da página onde ela pode ser encontrada (ex: Pag. 05).  
+#         Caso a resposta não esteja presente no documento, diga claramente que a informação não foi localizada.
+#         Priorize respostas claras, diretas e bem estruturadas. Utilize listas, tabelas, negrito, itálico e cabeçalhos Markdown sempre que apropriado.  
+#         Evite suposições ou interpretações além do que está explícito no documento.
+#         Contexto: {context}
+#     '''
 
-    messages = [('system', system_prompt)]
-    for message in st.session_state.messages:
-        messages.append((message.get('role'), message.get('content')))
-    messages.append(('human', '{input}'))
+#     messages = [('system', system_prompt)]
+#     for message in st.session_state.messages:
+#         messages.append((message.get('role'), message.get('content')))
+#     messages.append(('human', '{input}'))
 
-    prompt = ChatPromptTemplate.from_messages(messages)
+#     prompt = ChatPromptTemplate.from_messages(messages)
 
-    question_answer_chain = create_stuff_documents_chain(
-        llm=llm_model,
-        prompt=prompt,
-    )
-    chain = create_retrieval_chain(
-        retriever=retriever,
-        combine_docs_chain=question_answer_chain,
-    )
-    response = chain.invoke({'input': query})
-    return response.get('answer')
+#     question_answer_chain = create_stuff_documents_chain(
+#         llm=llm_model,
+#         prompt=prompt,
+#     )
+#     chain = create_retrieval_chain(
+#         retriever=retriever,
+#         combine_docs_chain=question_answer_chain,
+#     )
+#     response = chain.invoke({'input': query})
+#     return response.get('answer')
--- utils/process_vector.py
+++ utils/process_vector.py
@@ -1,27 +1,27 @@
-import os
+# import os
 
-from langchain_openai import OpenAIEmbeddings
+# from langchain_openai import OpenAIEmbeddings
 
-persist_directory = 'db'
+# persist_directory = 'db'
 
-def load_existing_vector_store():
-    from langchain_chroma import Chroma
-    if os.path.exists(os.path.join(persist_directory)):
-        vector_store = Chroma(
-            persist_directory=persist_directory,
-            embedding_function=OpenAIEmbeddings(),
-        )
-        return vector_store
-    return None
+# def load_existing_vector_store():
+#     from langchain_chroma import Chroma
+#     if os.path.exists(os.path.join(persist_directory)):
+#         vector_store = Chroma(
+#             persist_directory=persist_directory,
+#             embedding_function=OpenAIEmbeddings(),
+#         )
+#         return vector_store
+#     return None
 
-def add_to_vector_store(chunks, vector_store=None):
-    from langchain_chroma import Chroma
-    if vector_store:
-        vector_store.add_documents(chunks)
-    else:
-        vector_store = Chroma.from_documents(
-            documents=chunks,
-            embedding=OpenAIEmbeddings(),
-            persist_directory=persist_directory,
-        )
-    return vector_store
+# def add_to_vector_store(chunks, vector_store=None):
+#     from langchain_chroma import Chroma
+#     if vector_store:
+#         vector_store.add_documents(chunks)
+#     else:
+#         vector_store = Chroma.from_documents(
+#             documents=chunks,
+#             embedding=OpenAIEmbeddings(),
+#             persist_directory=persist_directory,
+#         )
+#     return vector_store
