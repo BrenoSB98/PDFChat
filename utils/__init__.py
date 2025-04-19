db7e5223edddeaf18a77e003f2a59bcc7dc34c13
Breno Braido <81990426+BrenoSB98@users.noreply.github.com>
2025-04-19 11:57:25 -0300

    Remove Comment Lines

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
-# from .process_document import process_pdf
-# from .process_vector import load_existing_vector_store, add_to_vector_store
-# from .llm_config import llm
+from .process_document import process_pdf
+from .process_vector import load_existing_vector_store, add_to_vector_store
+from .llm_config import llm
--- utils/process_document.py
+++ utils/process_document.py
@@ -1,22 +1,22 @@
-# import os
-# import tempfile
+import os
+import tempfile
 
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
 
-# def process_pdf(file):
-#     with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as temp_file:
-#         temp_file.write(file.read())
-#         temp_file_path = temp_file.name
+def process_pdf(file):
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as temp_file:
+        temp_file.write(file.read())
+        temp_file_path = temp_file.name
 
-#     loader = PyPDFLoader(temp_file_path)
-#     docs = loader.load()
+    loader = PyPDFLoader(temp_file_path)
+    docs = loader.load()
 
-#     os.remove(temp_file_path)
+    os.remove(temp_file_path)
 
-#     text_spliter = RecursiveCharacterTextSplitter(
-#         chunk_size=1000,
-#         chunk_overlap=400,
-#     )
-#     chunks = text_spliter.split_documents(documents=docs)
-#     return chunks
+    text_spliter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=400,
+    )
+    chunks = text_spliter.split_documents(documents=docs)
+    return chunks
